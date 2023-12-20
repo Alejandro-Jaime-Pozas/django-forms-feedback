404a5aef7362fcf3303930f4804dea19f96e481f
Alex Jaime <ajaimepozas@gmail.com>
2023-12-20 17:58:02 -0600

    specific form field handling

--- reviews/forms.py
+++ reviews/forms.py
@@ -1,4 +1,12 @@
 from django import forms
 
 class ReviewForm(forms.Form):
-    username = forms.CharField()
+    username = forms.CharField(
+        label='Your Name', 
+        max_length=100, 
+        error_messages={ # to allow specific error msgs to show user
+            'required': 'Your name must not be empty!',
+            'max_length': 'Please enter a shorter name!',
+        },
+        required=False # use this if not req field, default is true
+        )
