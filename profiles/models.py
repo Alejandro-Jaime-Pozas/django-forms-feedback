472f0147d81690e177ba28b0de68932b421933d6
Alex Jaime <ajaimepozas@gmail.com>
2023-12-22 13:49:15 -0600

    added model and views

--- profiles/models.py
+++ profiles/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+
+class UserProfile(models.Model):
+    image = models.FileField(upload_to='images') # this file should be stored on hard drive, not cloud storage, only the path should be stored in database; upload_to to work, need to add MEDIA_ROUTE in main project settings.py
--- profiles/views.py
+++ profiles/views.py
@@ -1,12 +1,51 @@
 from django.shortcuts import render
 from django.views import View
+from django.http import HttpResponseRedirect
+
+from .forms import ProfileForm
+from .models import UserProfile
 
 # Create your views here.
 
-
 class CreateProfileView(View):
     def get(self, request):
-        return render(request, "profiles/create_profile.html")
+        form = ProfileForm()
+        return render(request, "profiles/create_profile.html", {
+            'form': form
+        })
 
     def post(self, request):
-        pass
+        submitted_form = ProfileForm(request.POST, request.FILES)
+        if submitted_form.is_valid():
+            profile = UserProfile(image=request.FILES['user_image']) # this removes the need to write a file to disk
+            profile.save()
+            return HttpResponseRedirect('/profiles')
+        
+        return render(request, "profiles/create_profile.html", {
+            'form': submitted_form
+        })
+    
+
+# # basic static approach to store a file into hard disk (not ideal)
+# def store_file(file):
+#     with open('temp/me.jpg', 'wb+') as dest:
+#         for chunk in file.chunks():
+#             dest.write(chunk)
+    
+# # basic approach to uploading file
+# class CreateProfileView(View):
+#     def get(self, request):
+#         form = ProfileForm()
+#         return render(request, "profiles/create_profile.html", {
+#             'form': form
+#         })
+
+#     def post(self, request):
+#         submitted_form = ProfileForm(request.POST, request.FILES)
+#         if submitted_form.is_valid():
+#             store_file(request.FILES['image']) # gives access to the request's files, with html element name set to 'image'
+#             return HttpResponseRedirect('/profiles')
+        
+#         return render(request, "profiles/create_profile.html", {
+#             'form': submitted_form
+#         })
