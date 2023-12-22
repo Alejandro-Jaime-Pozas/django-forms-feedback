365fa0484cffbb0b823efad334b374d737cff2c3
Alex Jaime <ajaimepozas@gmail.com>
2023-12-22 09:48:55 -0600

    template, detail views

--- reviews/urls.py
+++ reviews/urls.py
@@ -3,6 +3,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.review),
-    path('thank-you', views.thank_you)
+    path('', views.ReviewView.as_view()), # this for class view
+    # path('', views.review), # this for fn view
+    # path('thank-you', views.thank_you), # this for fn view
+    path('thank-you', views.ThankYouView.as_view()), # this for class view
+    path('reviews', views.ReviewsListView.as_view()),
+    path('reviews/<int:id>', views.SingleReviewView.as_view()),
 ]
--- reviews/views.py
+++ reviews/views.py
@@ -1,11 +1,38 @@
+from typing import Any
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
+from django.views import View # checkout docs, multiple View classes
+from django.views.generic.base import TemplateView
 
 from .forms import ReviewForm
 from .models import Review
 
 # Create your views here.
 
+# review view as a class; django will handle incoming request based on function name ie 'get()'
+class ReviewView(View):
+
+    def get(self, request):
+        form = ReviewForm()
+        return render(request, 'reviews/index.html', {
+            'form': form
+        })
+
+    def post(self, request):
+        form = ReviewForm(request.POST) # request as parameter and its POST value to access the form's input contents
+        if form.is_valid(): # checks if user input is valid/not empty
+            print(form) # cleaned_data returns a dict
+            print(form.cleaned_data) # cleaned_data returns a dict
+            form.save() # since form is connected to model, can save form inputs directly
+            return HttpResponseRedirect('/thank-you')
+        
+        return render(request, 'reviews/index.html', {
+            'form': form
+        })
+
+
+
+# review view as a fn
 def review(request):
     # if method is post, do something with input and redirect to other page. for post requests, don't return a rendered html template, instead redirect
     if request.method == 'POST':
@@ -33,5 +60,40 @@ def review(request):
         'form': form
     })
 
+    
+# thank_you view as a template class
+class ThankYouView(TemplateView):
+    template_name = 'reviews/thank_you.html' # django auto returns get() req for url
+    # you should return the context that is exposed to template, so dict
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs) # this is a dict
+        context['message'] = 'This works!'
+        return context
+
+# # thank_you view as a class
+# class ThankYouView(View):
+#     def get(self, request):
+#         return render(request, 'reviews/thank_you.html')
+
+# thank_you view as a fn
 def thank_you(request):
     return render(request, 'reviews/thank_you.html')
+
+
+class ReviewsListView(TemplateView):
+    template_name = 'reviews/review_list.html' # django auto returns get() req for url
+    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        reviews = Review.objects.all()
+        context['reviews'] = reviews
+        return context
+    
+
+class SingleReviewView(TemplateView):
+    template_name = 'reviews/single_review.html'
+
+    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        context['review'] = Review.objects.get(id=kwargs['id']) # can get kwargs from path() which contains the 'id' in its url
+        print(kwargs)
+        return context 
